2bb3e8e202baefce3ca109b95a7b0636d614574c
Klonochkin <di630538@yandex.ru>
2024-10-24 09:22:15 +0300

    impr: Заменяет число и месяц на единый объект типа Date

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from 'react';
 import './index.css';
 
-interface Holiday {
-    holidays: {
-        text: string;
-        pages: {
-            title: string;
-            content_urls: {
-                desktop: {
-                    page: string;
-                };
-            };
-        }[];
-    }[];
+interface HolidayInfo {
+    text: string;
+    pages: Array<
+        | {
+              title: string;
+              content_urls: {
+                  desktop: {
+                      page: string;
+                  };
+              };
+          }
+        | undefined
+    >;
 }
 
-const month: string[] = [
+interface HolidayData {
+    holidays: HolidayInfo[];
+}
+
+const months = [
     'Январь',
     'Февраль',
     'Март',
@@ -30,136 +35,119 @@ const month: string[] = [
     'Декабрь',
 ];
 
-const today = new Date();
-
 function HolidayCard({
     text,
     link,
     linkTitle,
 }: {
     text: string;
-    link: string;
-    linkTitle: string;
+    link?: string;
+    linkTitle?: string;
 }) {
     return (
-        <div>
+        <li>
             <p>{text}</p>
-            <a href={link} target='_blank'>
-                {linkTitle}
-            </a>
-        </div>
+            {link && linkTitle && (
+                <a href={link} className='link' target='_blank'>
+                    {linkTitle}
+                </a>
+            )}
+        </li>
     );
 }
 
-function HolidayList({ data }: { data?: Holiday }) {
-    if (data?.holidays && data?.holidays.length > 0) {
+function HolidayList({ data }: { data?: HolidayInfo[] }) {
+    if (data && data.length > 0) {
         return (
             <ul>
-                {data.holidays.map((holiday, i) => (
+                {data.map(({ text, pages: [page] }, i) => (
                     <HolidayCard
                         key={i}
-                        text={holiday?.text}
-                        link={holiday?.pages[0]?.content_urls?.desktop?.page}
-                        linkTitle={holiday?.pages[0]?.title}></HolidayCard>
+                        text={text}
+                        link={page?.content_urls.desktop.page}
+                        linkTitle={page?.title}
+                    />
                 ))}
             </ul>
         );
-    } else {
-        return <p>Праздников нету</p>;
     }
+
+    return <p>Праздников нету</p>;
 }
 
 function App() {
-    const [currentDay, setCurrentDay] = useState<number>(today.getDate());
-    const [currentMonth, setCurrentMonth] = useState(today.getMonth());
-    const [data, setData] = useState<Holiday>();
+    const today = new Date();
+    const [data, setData] = useState<HolidayInfo[]>();
     const [daysInCurrentMonth, setdaysInCurrentMonth] = useState<number[]>();
-    const [date] = useState(today);
+    const [date, setNewDate] = useState(today);
 
     useEffect(() => {
-        if (currentDay > new Date(2020, currentMonth + 1, 0).getDate()) {
-            setCurrentDay(new Date(2020, currentMonth + 1, 0).getDate());
-            console.log(currentMonth);
-            date.setDate(currentDay);
-            date.setMonth(currentMonth);
-            return;
-        }
         getHolidays();
-        date.setMonth(currentMonth);
-        date.setDate(currentDay);
-    }, [currentDay, currentMonth]);
+    }, [date]);
 
     useEffect(() => {
         setdaysInCurrentMonth(
             Array.from({
-                length: new Date(2020, currentMonth + 1, 0).getDate(),
+                length: new Date(2020, date.getMonth() + 1, 0).getDate(),
             }).map((_, i) => ++i),
         );
-    }, [currentMonth]);
+    }, [date.getMonth()]);
 
     function nextDay() {
-        date.setDate(date.getDate() + 1);
-        setCurrentDay(date.getDate());
-        setCurrentMonth(date.getMonth());
+        setNewDate(new Date(date.setDate(date.getDate() + 1)));
     }
 
     function prevDay() {
-        date.setDate(date.getDate() - 1);
-        setCurrentDay(date.getDate());
-        setCurrentMonth(date.getMonth());
+        setNewDate(new Date(date.setDate(date.getDate() - 1)));
     }
 
     function getHolidays() {
-        const url = `https://ru.wikipedia.org/api/rest_v1/feed/onthisday/holidays/${currentMonth + 1}/${currentDay}`;
+        const url = `https://ru.wikipedia.org/api/rest_v1/feed/onthisday/holidays/${date.getMonth() + 1}/${date.getDate()}`;
         fetch(url)
             .then((response) => response.json())
-            .then((res) => setData(res));
+            .then(({ holidays }: HolidayData) => setData(holidays));
     }
 
     return (
-        <>
-            <div className='container'>
-                <button className='container__button' onClick={() => prevDay()}>
-                    назад
-                </button>
-                <select
-                    value={currentDay}
-                    name='1'
-                    id='1'
-                    className='container__select'
-                    onChange={(event) => {
-                        setCurrentDay(Number(event.target.value));
-                    }}>
-                    {daysInCurrentMonth?.map((day, i) => (
-                        <option value={day} key={i}>
-                            {day}
-                        </option>
-                    ))}
-                </select>
-                <button className='container__button' onClick={() => nextDay()}>
-                    вперёд
-                </button>
-
-                <select
-                    value={currentMonth}
-                    name='2'
-                    id='2'
-                    className='container__select'
-                    onChange={(event) => {
-                        setCurrentMonth(Number(event.target.value));
-                    }}>
-                    {month?.map((day, i) => (
-                        <option value={i} key={i}>
-                            {day}
-                        </option>
-                    ))}
-                </select>
-            </div>
-
-            <div>
-                <HolidayList data={data} />
-            </div>
-        </>
+        <main>
+            <button className='button' onClick={() => prevDay()}>
+                назад
+            </button>
+            <select
+                value={date.getDate()}
+                name='day'
+                className='select'
+                onChange={(event) => {
+                    setNewDate(
+                        new Date(date.setDate(Number(event.target.value))),
+                    );
+                }}>
+                {daysInCurrentMonth?.map((day, i) => (
+                    <option value={day} key={i}>
+                        {day}
+                    </option>
+                ))}
+            </select>
+            <button className='button' onClick={() => nextDay()}>
+                вперёд
+            </button>
+            <select
+                value={date.getMonth()}
+                name='month'
+                className='select'
+                onChange={(event) => {
+                    setNewDate(
+                        new Date(date.setMonth(Number(event.target.value))),
+                    );
+                }}>
+                {months?.map((day, i) => (
+                    <option value={i} key={i}>
+                        {day}
+                    </option>
+                ))}
+            </select>
+            <HolidayList data={data} />
+        </main>
     );
 }
 
